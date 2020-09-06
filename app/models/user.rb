f3d28f63a0f5dc9cabba6caad865fca653fdd3ad
Kohei-1729 <specialweek52nd@yahoo.co.jp>
2020-09-06 21:23:19 +0900

    全角、全角カナvalidatesした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  with_options presence: true do
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name_kana, :first_name_kana, format: { /\A[ァ-ヶー－]+\z/}
+  end
 end
