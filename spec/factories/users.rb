1a516fcdcbcb4baf019aa37df25a8aeb58b47b6f
Kohei-1729 <specialweek52nd@yahoo.co.jp>
2020-09-08 12:47:42 +0900

    これからテストコード 修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     nickname { 'Furima太郎' }
     email { 'test@test' }
     password { 'aa000000'}
-    confirmation_password { 'aa000000' }
+    password_confirmation { 'aa000000' }
     last_name { '振馬' }
     first_name { '太郎' }
     last_name_kana { 'フリマ' }
     first_name_kana { 'タロウ' }
-    birth-date { '1990-07-10'}
+    birth_date { '1990-07-10'}
   end
 end
