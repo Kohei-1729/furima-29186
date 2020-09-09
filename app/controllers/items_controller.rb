5999d0ffb1156c635b243a2b64da32420c00034f
Kohei-1729 <specialweek52nd@yahoo.co.jp>
2020-09-09 11:18:21 +0900

    ActiveHash実装成功

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,20 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def created
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def user
@@ -9,7 +23,10 @@ class ItemsController < ApplicationController
   def destroy
   end
 
-  def new
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_id, :price)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :handling_time
+
+  validates :category_id, numericality: { other_than: 0 }
+  validates :condition_id, numericality: { other_than: 0 }
+  validates :postage_payer_id, numericality: { other_than: 0 }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :handling_time_id, numericality: { other_than: 0 } 
+  
+
 end
