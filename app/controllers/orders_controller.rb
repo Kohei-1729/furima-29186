4c8676046d32cf12cc5dc78b9146c98dfa5a0042
Kohei-1729 <specialweek52nd@yahoo.co.jp>
2020-09-13 14:58:39 +0900

    ビューの一応の実装
    oders/index.html.rbのクレジットカード以外の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,20 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:zip_code, :prefecture_id, :city, :street_id, :building_name, :phone_number) .merge(user_id: current_user.id)
   end
 end
