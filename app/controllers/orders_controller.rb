830a72ade4522c7895444c166a322a5652beb637
Kohei-1729 <specialweek52nd@yahoo.co.jp>
2020-09-14 13:01:19 +0900

    Formオブジェクトは完成か!?
    Token can't be blank

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < ApplicationController
   def index
+    @address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
-    @address = Address.new(address_params)
-    if @address.save
+    @item = Item.find(params[:item_id])
+    @address = PurchaseAddress.new(address_params)
+    if @address.valid?
+      pay_item
+      @address.save
       redirect_to root_path
     else
       render :index
@@ -15,6 +19,19 @@ class OrdersController < ApplicationController
   private
 
   def address_params
-    params.require(:address).permit(:zip_code, :prefecture_id, :city, :street_id, :building_name, :phone_number) .merge(user_id: current_user.id)
+    params.permit(:user_id, :item_id, :zip_code, :prefecture_id, :city, :street_number, :building_name, :phone_number) .merge(user_id: current_user.id)
+  end
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 end
---
+++ app/forms/purchase_address.rb
@@ -0,0 +1,27 @@
+class PurchaseAddress
+
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :zip_code, :prefecture_id, :city, :street_number, :building_name, :phone_number
+
+  attr_accessor :token
+  with_options presence: true do
+    validates :zip_code
+    validates :prefecture_id
+    validates :city
+    validates :street_number
+    validates :phone_number
+    validates :token
+  end
+
+  validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
+  validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "can't be blank" }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "can't be blank"}
+
+  def save
+    # purchaseの情報を保存し、「purchase」という変数に入れている
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    # addressの情報を保存
+    Address.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, street_number: street_number, building_name: building_name, phone_number: phone.number, purchase_id: purchase.id)
+  end
+end
