c56d7f47b1291938516d8acb1a4886b6c8339aba
Kohei-1729 <specialweek52nd@yahoo.co.jp>
2020-10-05 18:24:03 +0900

    トップページでカテゴリー欄を表示/非表示、実装成功

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,9 +1,13 @@
 function showCategoryList(){
   const categoryBtn = document.getElementsByClassName("category-list")[0]
+  const categoryList = document.getElementById("pull-down")
 
   categoryBtn.addEventListener('click', function(){
-    console.log("click OK")
+    if (categoryList.getAttribute("style") == "display:none;"){
+      categoryList.removeAttribute("style", "display:none;")
+    }else {
+      categoryList.setAttribute("style", "display:none;")
+    }
   })
-
 }
 window.addEventListener('load', showCategoryList)
